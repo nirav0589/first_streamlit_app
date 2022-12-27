3fef466c3f718846a4b1b3c24a54a747aa3e8126
nirav0589 <121510554+nirav0589@users.noreply.github.com>
2022-12-27 18:34:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response,json())
+streamlit.text(fruityvice_response.json())
