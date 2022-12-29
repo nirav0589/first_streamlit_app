03f747d43d211aefcd1d1f2dc81d1c746e8320e7
nirav0589 <121510554+nirav0589@users.noreply.github.com>
2022-12-29 13:12:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,9 @@ streamlit.dataframe (fruits_to_show)
 
 streamlit. header('Fruityvice Fruit Advice!')
 
-def get_fruityvice_data(fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas. json_normalize(fruityvice_response.json())
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized 
 
 streamlit.header('Fruityvice Fruit Advice!')
