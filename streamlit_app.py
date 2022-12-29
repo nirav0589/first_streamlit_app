5f1a0e9d42fbccc1e18a9cb2d68c10e645e0f47a
nirav0589 <121510554+nirav0589@users.noreply.github.com>
2022-12-29 10:12:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 streamlit. header('Fruityvice Fruit Advice!')
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  fruityvice_normalized = pandas. json_normalize(fruityvice_response.json ())
+  fruityvice_normalized = pandas. json_normalize(fruityvice_response.json())
   return fruityvice_normalized 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
